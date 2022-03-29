34fa02b942bfbf4c5e69ea7f0e75b8d0fa008919
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-03-29 13:11:58 +0530

    code part

--- BubbleSort/src/com/Six.java
+++ BubbleSort/src/com/Six.java
@@ -2,5 +2,21 @@ package com;
 
 public class Six {
     // STABLE SORTING ALGORITHM :
-    
+    // ans - order should be the same when the value is the same
+    //  in short :  when the original order is maintained for values that are equal -->  stable sorting algo
+    // else if not maintained --> unstable sorting algo
+    // ie when the order is not maintained when two or more values are the same then it is unstable sorting algo
+
+    // here r - red ball
+    // here b - black ball
+    // ex array - 10b , 20b , 10r,20r,30b
+    // stable sorting algo which is bubble sort
+    // 10b , 10r , 20b , 20r ,30b
+    // unstable sorting algo
+    // 10r,10b,20r,20b,30b
+    // here the order has been changed as you can see despite having the same value
+    // so unstable sorting algo
+
 }
+
+
